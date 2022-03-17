785a4a672033aef0d35e7b5096cb7050f4f8f7f5
wakshumA <gizawamante2006@gmail.com>
2022-03-16 23:21:38 -0500

    Thymleaf class

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydio/bootStrap/DataGenerator.java
@@ -0,0 +1,24 @@
+package com.cydio.bootStrap;
+
+import com.cydio.model.Student;
+import com.github.javafaker.Faker;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<Student> createStudent(){
+
+        List<Student> students = Arrays.asList(
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state())
+        );
+
+        return students;
+
+    }
+}
---
+++ Spring-09-Spring-Thymeleaf/src/test/java/com/cydio/Spring09SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09SpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
