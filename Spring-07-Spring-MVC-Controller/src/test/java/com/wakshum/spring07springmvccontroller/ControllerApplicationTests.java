991ea7625187eb52d5962d94f3bef5cd1625767c
wakshumA <gizawamante2006@gmail.com>
2022-04-06 22:40:06 -0500

    mvc class
    q
    git status
    '

---
+++ Spring-07-Spring-MVC-Controller/src/test/java/com/wakshum/spring07springmvccontroller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakshum.spring07springmvccontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
