9a82836db585a08de1f66660a2d74ae70a064bdd
wakshumA <gizawamante2006@gmail.com>
2022-04-13 21:16:25 -0500

    database class

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/wakshum/entity/Student.java
@@ -0,0 +1,16 @@
+package com.wakshum.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    private Long id;
+
+    private String firstName;  //first_name
+    private String lastName;
+    private String email;
+
+}
